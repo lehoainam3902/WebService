aa42c85e51e3423fb3aabcab9e9cdb9ca2c58a02
lehoainam3902 <lehoainam1233902@gmail.com>
2024-01-02 19:18:24 +0700

    ratingcontroller

--- src/main/java/com/nhom1/controller/RatingController.java
+++ src/main/java/com/nhom1/controller/RatingController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,7 @@ public class RatingController {
 	@Autowired
 	private RatingService ratingService;
 
+	@PostMapping("/create")
 	public ResponseEntity<Rating> createRating(@RequestBody RatingRequest req,
 			@RequestHeader("Authorization") String jwt) throws UserExCeption, ProductException{
 		
